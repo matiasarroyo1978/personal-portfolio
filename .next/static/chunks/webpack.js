017159fe31db83ca0129aabdfadafa141b1ddbc8
matias <matiasarroyo684@gmail.com>
2023-07-14 15:32:05 -0300

    about terminado

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "58cfb174b9ef87be"; }
+/******/ 		__webpack_require__.h = function() { return "6f5d08c70d6cd86c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/about/index.js
+++ pages/about/index.js
@@ -94,19 +94,49 @@ const About = () => {
       </motion.div>
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
         <div className="flex-1 flex flex-col justify-center">
-          <h2 className="h2">Captivating <span className="text-accent">stories</span> birth magnificient designs.</h2>
-          <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">
+          <motion.h2 
+             variants={fadeIn('right',0.2)} 
+             initial="hidden" 
+             animate="show" 
+             exit="hidden" 
+             className="h2"
+          >Captivating 
+            <span className="text-accent">
+              stories
+            </span> birth magnificient designs.
+          </motion.h2>
+          <motion.p 
+            variants={fadeIn('right',0.4)} 
+            initial="hidden" 
+            animate="show" 
+            exit="hidden" 
+            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
+            >
             2 years ago, I started my journey as a web developer. I worked on a few projects and learned a lot about the industry.
-          </p>
-          <div>
+          </motion.p>
+          <motion.div 
+            variants={fadeIn('right',0.6)} 
+            initial="hidden" 
+            animate="show" 
+            exit="hidden" 
+            className="hidden md:flex md:max-x-xl xl:max-w-none mx-auto xl:mx-0 mb-8">
             <div>
-              <div>
-
+              <div className="reltive flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end ={10} duration={5}/> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Years of experience</div>
               </div>
             </div>
-          </div>
+          </motion.div>
         </div>
-        <div className="flex- flex-col w-full xl:max-w-[48%] h-[480px]"></div>
+        <motion.div 
+          variants={fadeIn('left',0.4)} 
+          initial="hidden" 
+          animate="show" 
+          exit="hidden" 
+          className="flex- flex-col w-full xl:max-w-[48%] h-[480px]">
+        </motion.div>
         <div>
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
