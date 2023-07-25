56877da05cd23f84f951f90236a0aca1a361c4b4
matias <matiasarroyo684@gmail.com>
2023-07-25 18:32:12 -0300

    work

--- pages/services/index.js
+++ pages/services/index.js
@@ -78,15 +78,12 @@ const Services = () => {
               My Services{' '}
               <span className='text-accent'>.</span>
             </motion.h2>
-            <div
-              className='max-w-[450px] mx-auto lg:mx-0 text-md overflow-y-auto max-h-[400px]'
-            >
             <motion.p
               variants={fadeIn('up', 0.4)}
               initial='hidden'
               animate='show'
               exit='hidden'
-              className='max-w-[450px] mx-auto lg:mx-0 text-md'
+              className='max-w-[450px] mx-auto lg:mx-0 text-md overflow-y-auto max-h-[400px]'
             >
               I specialize in building responsive and user-friendly websites using the latest technologies such as HTML5,
               CSS3, React, Next.js, and JavaScript. Whether you need a simple static website or a complex web application.
@@ -99,7 +96,6 @@ const Services = () => {
               vital for successful project outcomes. I am always open to feedback, and I strive to understand your
               specific requirements to deliver the best results.
             </motion.p>
-            </div>
           </div>
           <motion.div
             variants={fadeIn('down', 0.6)}
